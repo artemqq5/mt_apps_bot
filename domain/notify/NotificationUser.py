ebb683724c5d0393dea972fc6836b973cd2370f2
artem <artemqq5@gmail.com>
2024-05-09 17:59:00 +0300

    notification localization

--- domain/notify/NotificationUser.py
+++ domain/notify/NotificationUser.py
@@ -3,7 +3,7 @@ from aiogram.exceptions import TelegramForbiddenError
 from aiogram_i18n import I18nContext
 from aiogram_i18n.types import ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
 
-from data.repositoryDB.AccessRepository import AccessRepository
+from data.constants.access import ACTIVE_APP_STATUS, BANNED_APP_STATUS, DRAFT_APP_STATUS
 from data.repositoryDB.UserRepository import UserRepository
 
 
@@ -17,7 +17,7 @@ class NotificationUser:
 
         for user in users:
             try:
-                await self.generate_message_users(data, bot, user)
+                await self.generate_message_users(data, bot, user, i18n)
                 counter += 1
             except TelegramForbiddenError as e:
                 block += 1
@@ -29,8 +29,12 @@ class NotificationUser:
         print(f"notification {error_message}: {counter}/{len(users)}")
         return i18n.NOTIFY.RESULT(get=str(counter), users=str(len(users)), block=str(block), other=str(other))
 
-    @staticmethod  # Розсилка за id (текс, медіа, кнопка)
-    async def generate_message_users(data, bot: Bot, user_id):
+    # Розсилка за id (текс, медіа, кнопка)
+    async def generate_message_users(self, data, bot: Bot, user_id, i18n=None):
+        if i18n:
+            await self.generate_message_state(data, bot, user_id, i18n)
+            return
+
         keyboard = ReplyKeyboardRemove()
         if data.get('button_url', None):
             keyboard = InlineKeyboardMarkup(inline_keyboard=[
@@ -47,6 +51,20 @@ class NotificationUser:
         else:
             await bot.send_message(chat_id=user_id, text=data['message'], reply_markup=keyboard)
 
+    @staticmethod  # Розсилка за id (текс, медіа, кнопка)
+    async def generate_message_state(data, bot: Bot, user, i18n):
+        with i18n.use_locale(user['lang']):
+            if data['type'] == ACTIVE_APP_STATUS:
+                message = i18n.NOYIFY.APP_STATUS_ACTIVE(app_name=data['app_name'])
+            elif data['type'] == BANNED_APP_STATUS:
+                message = i18n.NOYIFY.APP_STATUS_BAN(app_name=data['app_name'])
+            elif data['type'] == DRAFT_APP_STATUS:
+                message = i18n.NOYIFY.APP_STATUS_DRAFT(app_name=data['app_name'])
+            else:
+                raise Exception
+
+            await bot.send_photo(chat_id=user['telegram_id'], photo=data['photo'], caption=message)
+
     # Стандартна розсилка користувачам
     async def default_messaging_users(self, bot: Bot, i18n: I18nContext, data):
         if data['category'] == i18n.NOTIFY.CATEGORY_NO_USERS():
@@ -54,35 +72,39 @@ class NotificationUser:
         elif data['category'] == i18n.NOTIFY.CATEGORY_USERS():
             users = [user['telegram_id'] for user in UserRepository().get_users_in_team() if user['banned'] == 0]
         elif data['category'] == i18n.TEAM_MESSAGING():
-            users = [user['telegram_id'] for user in UserRepository().get_users_by_team_id(data['team_id']) if user['banned'] == 0]
+            users = [user['telegram_id'] for user in UserRepository().get_users_by_team_id(data['team_id']) if
+                     user['banned'] == 0]
         else:
             users = [user['telegram_id'] for user in UserRepository().get_all_users() if user['banned'] == 0]
 
         return await self.__notify_list_any(bot, users, data, "default user message", i18n)
 
-    async def ban_app_message(self, application, bot, i18n):
-        users = [user['telegram_id'] for user in UserRepository().get_all_users() if user['banned'] == 0]
+    async def ban_app_message(self, application, bot: Bot, i18n):
+        users = [user for user in UserRepository().get_all_users() if user['banned'] == 0]
         data = {
             "photo": application['image'],
-            "message": i18n.NOYIFY.APP_STATUS_BAN(app_name=application['name'])
+            "app_name": application['name'],
+            "type": BANNED_APP_STATUS
         }
 
         return await self.__notify_list_any(bot, users, data, "ban_app_message", i18n)
 
-    async def active_app_message(self, application, bot, i18n):
-        users = [user['telegram_id'] for user in UserRepository().get_all_users() if user['banned'] == 0]
+    async def active_app_message(self, application, bot: Bot, i18n):
+        users = [user for user in UserRepository().get_all_users() if user['banned'] == 0]
         data = {
             "photo": application['image'],
-            "message": i18n.NOYIFY.APP_STATUS_ACTIVE(app_name=application['name'])
+            "app_name": application['name'],
+            "type": ACTIVE_APP_STATUS
         }
 
         return await self.__notify_list_any(bot, users, data, "active_app_message", i18n)
 
-    async def draft_app_message(self, application, bot, i18n):
-        users = [user['telegram_id'] for user in UserRepository().get_all_users() if user['banned'] == 0]
+    async def draft_app_message(self, application, bot: Bot, i18n):
+        users = [user for user in UserRepository().get_all_users() if user['banned'] == 0]
         data = {
             "photo": application['image'],
-            "message": i18n.NOYIFY.APP_STATUS_DRAFT(app_name=application['name'])
+            "app_name": application['name'],
+            "type": DRAFT_APP_STATUS
         }
 
         return await self.__notify_list_any(bot, users, data, "draft_app_message", i18n)
